124bb461064703da8d37afb1465f87a40cdbfc45
RedarsHubi <redamasters@gmail.com>
2024-04-24 15:28:32 +0100

    Changed to array.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,6 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([pres, ures]) => { console.log(pres.body, ures.firstName, ures.lastName); })
+    .then((res) => { console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`); })
     .catch(() => console.log('Signup system offline'));
 }
