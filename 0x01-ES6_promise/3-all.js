ac04d0ebe04d74996b7950bce75f958427629fe7
RedarsHubi <redamasters@gmail.com>
2024-04-24 15:22:34 +0100

    Return Promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([pres, ures]) => { console.log(pres.body, ures.firstName, ures.lastName); })
     .catch(() => console.log('Signup system offline'));
 }
