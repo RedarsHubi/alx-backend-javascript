cc1aa67a0fe4961e9da3b6d214c9c3e7f57b7d12
RedarsHubi <redamasters@gmail.com>
2024-06-20 12:00:57 +0100

    4

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,21 +1,34 @@
 const assert = require('assert');
-const calculateNumber = require('./1-calcul.js');
+const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function () {
-    it('returns 3', function () {
-      assert.strictEqual(calculateNumber('SUM', 1.9, 1.1), 3);
-    });
-    it('returns 5', function () {
-      assert.strictEqual(calculateNumber('SUBSTRACT', 4.6, 0.4), 5);
+describe('calculateNumber', function() {
+    describe('SUM', function() {
+        it('should return 6 when inputs are 1.4 and 4.5', function() {
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        });
+        it('should return 0 when inputs are -1.4 and 1.4', function() {
+            assert.strictEqual(calculateNumber('SUM', -1.4, 1.4), 0);
+        });
     });
 
-    it('returns -2', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', -1.6, -0.6), 2);
+    describe('SUBTRACT', function() {
+        it('should return -4 when inputs are 1.4 and 4.5', function() {
+            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+        it('should return 4 when inputs are 4.7 and 1.2', function() {
+            assert.strictEqual(calculateNumber('SUBTRACT', 4.7, 1.2), 4);
+        });
     });
-    it('returns error', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', -1.6, 0), 'Error');
+
+    describe('DIVIDE', function() {
+        it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        });
+        it('should return "Error" when inputs are 1.4 and 0', function() {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        });
+        it('should return "Error" when inputs are 1.4 and 0.2', function() {
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0.2), 'Error');
+        });
     });
-    it('returns 4', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 16, 4), 4);
-    });
-  })
+});
