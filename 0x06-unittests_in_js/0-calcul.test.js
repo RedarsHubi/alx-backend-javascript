87ab31b0afc0869cf1bcf0ed208b72a570d09ed2
RedarsHubi <redamasters@gmail.com>
2024-06-20 11:18:22 +0100

    1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,6 +3,13 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
     it('returns 3', function () {
-        assert.strictEqual(calculateNumber(1.9, 1.1), 3);
+      assert.strictEqual(calculateNumber(1.9, 1.1), 3);
+    });
+    it('returns 0 when inputs are -0.4 and 0.4', function () {
+      assert.strictEqual(calculateNumber(-0.4, 0.4), 0);
+    });
+
+    it('returns -2 when inputs are -1.6 and -0.4', function () {
+      assert.strictEqual(calculateNumber(-1.6, -0.4), -2);
     });
   })
